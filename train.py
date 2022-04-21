fe9b275facce729f9073b7580e3d9f0c607b87a9
Artem <yatsenkoa2020@gmail.com>
2022-04-21 13:36:35 -0600

    delete old yolo ):

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 from utils.general import read_cfg, create_model
 from model.yolov3 import YOLOV3
+import torch
 
 
 cfg = read_cfg('cfg/yolov3.cfg')
 
 
 yolo = YOLOV3(cfg)
+
+
+x = torch.zeros((1, 3, 320, 320))
+yolo(x)
