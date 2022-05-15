c7027e549be4949138448508d792299bd6c23f7b
yatsenkoa <yatsenkoa2020@gmail.com>
2022-05-15 14:50:45 -0400

    some progress on training func

---
+++ train.py
@@ -0,0 +1,16 @@
+from model.yolov3 import YOLOV3, train
+from utils.general import read_cfg
+
+im_to_detect = 'samples/street.jpg'
+
+cfg = read_cfg('cfg/yolov3.cfg')
+yolo = YOLOV3(cfg)
+
+yolo.summary()
+
+# finetune on pretrained weights
+yolo.load_weights('weights/yolov3.weights')
+
+# when you finetune, the model freezes the ResNet50 weights, and only trains the YOLO parts for faster training
+yolo.train(epochs=15, finetune=True)
+
