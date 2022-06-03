cc5110bdb5b781d3e91cba906b876ecfada17e00
yatsenkoa <yatsenkoa2020@gmail.com>
2022-06-03 19:16:18 -0400

    add data folder

---
+++ data/download_coco.sh
@@ -0,0 +1,4 @@
+#wget http://images.cocodataset.org/zips/train2017.zip train2017
+wget http://images.cocodataset.org/zips/val2017.zip val2017
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip annotations
+
