9704a2de49bb09d0a04774ead05ccb4827f67120
yatsenkoa <yatsenkoa2020@gmail.com>
2022-06-08 22:38:35 -0400

    minor change

--- detect.py
+++ detect.py
@@ -1,13 +1,16 @@
 from model.yolov3 import YOLOV3
 from utils.general import read_cfg
+import torch 
 
 im_to_detect = 'samples/street.jpg'
 
 cfg = read_cfg('cfg/yolov3.cfg')
 yolo = YOLOV3(cfg)
 
+yolo.load_state_dict(torch.load('weights/saved_model.pt'))
+
 yolo.summary()
-yolo.load_weights('weights/yolov3.weights')
+#yolo.load_weights('weights/yolov3.weights')
 
 dets = yolo.detect(im_to_detect, preview=False, save_img=True)
 
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from model.yolov3 import YOLOV3
 from utils.general import read_cfg
+import torch
 
 im_to_detect = 'samples/street.jpg'
 
@@ -11,6 +12,12 @@ yolo = YOLOV3(cfg)
 # finetune on pretrained weights
 yolo.load_weights('weights/yolov3.weights')
 
+yolo.freeze()
+
 # when you finetune, the model freezes the ResNet50 weights, and only trains the YOLO parts for faster training
 yolo._train('data/val2017', 'data/annotations',
-           epochs=15, finetune=True, num_iterations=10)
+           epochs=1, finetune=True, num_iterations=10)
+
+yolo.unfreeze()
+
+torch.save(yolo.state_dict(), 'weights/saved_model.pt')
